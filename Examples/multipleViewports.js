9c493570289d73dd7636e84c96c3a7384b61dce5
Bruce MacLeod <macleod@maine.edu>
2016-09-11 10:27:43 -0400

    Added a viewport example

---
+++ Examples/multipleViewports.js
@@ -0,0 +1,126 @@
+"use strict";
+
+var gl;
+var points;
+var program;
+
+window.onload = function init(){
+    var canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" );
+    }
+    // Four Vertices
+
+    //  Configure WebGL
+
+    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    program.vertexPositionAttribute = gl.getAttribLocation(program, "vPosition");
+    gl.enableVertexAttribArray(program.vertexPositionAttribute);
+
+    program.vertexColorAttribute = gl.getAttribLocation(program, "aVertexColor");
+    gl.enableVertexAttribArray(program.vertexColorAttribute);
+
+    // Load the data into the GPU
+
+    initBuffers();
+
+    render(canvas);
+};
+
+var triangleVertexPositionBuffer;
+var squareVertexPositionBuffer;
+var triangleVertexColorBuffer;
+var squareVertexColorBuffer;
+
+function initBuffers() {
+
+    triangleVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+
+    var vertices = [
+        -0.5,  0.5,  0.0,
+        -1.0, -0.5,  0.0,
+        0.0, -0.5,  0.0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    triangleVertexPositionBuffer.itemSize = 3;
+    triangleVertexPositionBuffer.numItems = 3;
+
+    triangleVertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
+    var colors = [
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    triangleVertexColorBuffer.itemSize = 4;
+    triangleVertexColorBuffer.numItems = 3;
+
+    squareVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+
+    vertices = [
+        0.75,  0.75,  0.0,
+        0.0,  0.75,  0.0,
+        0.75, 0.0,  0.0,
+        0.0, 0.0,  0.0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+    squareVertexPositionBuffer.itemSize = 3;
+    squareVertexPositionBuffer.numItems = 4;
+
+    squareVertexColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
+    colors = [];
+    for (var i=0; i < 4; i++) {
+        colors = colors.concat([0.5, 0.5, 1.0, 1.0]);
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    squareVertexColorBuffer.itemSize = 4;
+    squareVertexColorBuffer.numItems = 4;
+
+}
+
+function drawTriangle()
+{
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+    gl.vertexAttribPointer(program.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
+    gl.vertexAttribPointer(program.vertexColorAttribute, triangleVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+
+}
+
+function drawSquare()
+{
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+    gl.vertexAttribPointer(program.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexColorBuffer);
+    gl.vertexAttribPointer(program.vertexColorAttribute, squareVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+
+}
+function render(canvas) {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.viewport( canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2 );
+    drawTriangle();
+
+    gl.viewport(0, 0, canvas.width/2, canvas.height/2 );
+    drawSquare();
+
+    gl.viewport(0, canvas.height/2, canvas.width/2, canvas.height/2 );
+    drawTriangle();
+    drawSquare();
+
+}
